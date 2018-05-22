6f7fffa9542af70ee341856112830e417e2b03e9
DESKTOP-U7QL9FC\Administrator <254862688@qq.com>
2018-05-22 10:08:36 +0800

    ECS

---
+++ Assets/ECS/Example/BeginnerExample/SumFloatSystem.cs
@@ -0,0 +1,16 @@
+﻿using ECS;
+
+namespace ECSExample.Beginner {
+    [DebugSystemGroup("Sum")]
+    class SumFloatSystem : ComponentSystem {
+
+        [InjectTuple]
+        ComponentArray<FloatComponent> floats;
+        public override void OnUpdate() {
+            float sum = 0;
+            for (int i = 0; i < floats.Length; i++) {
+                sum += floats[i].value;
+            }
+        }
+    }
+}
