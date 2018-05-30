68439c090a463f7b46c35653498d5c9eb5e0d63d
DESKTOP-U7QL9FC\Administrator <254862688@qq.com>
2018-05-31 01:27:57 +0800

    2d关节组件说明案例

---
+++ Assets/Joint2D/Readme.txt
@@ -0,0 +1,2 @@
+https://blog.csdn.net/rickshaozhiheng/article/details/78509632
+2d关节组件的一些案例
---
+++ Assets/SuskeTest/ActionTest/ActionTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ActionTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Action<int> action = Test();
+        action(10);
+        action(100);
+        action(1000);
+	}
+
+
+    Action<int> Test()
+    {
+        int count = 0;
+        Action<int> action = delegate (int number)
+        {
+            count += number;
+            Debug.Log(count);
+        };
+
+        action(1);
+        return action;
+    }
+}
