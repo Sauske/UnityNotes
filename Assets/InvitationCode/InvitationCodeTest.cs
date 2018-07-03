1a6fd1f01c06259dc4b29f48a482c466e1fb50bf
DESKTOP-U7QL9FC\Administrator <254862688@qq.com>
2018-07-03 23:15:28 +0800

    invitationCode
    邀请码

---
+++ Assets/InvitationCode/InvitationCodeTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InvitationCode
+{
+    public class InvitationCodeTest : MonoBehaviour {
+
+        void Start() {
+            long id = 531347133124609;
+            string code = Invitation.IdToCode(id);
+
+            Debug.LogFormat("输入的ID是 '{0}'， 生成的邀请码是 '{1}'", id, code);
+            Debug.LogFormat("邀请码 '{0}' 转化后的ID是 '{1}'", code, Invitation.CodeToId(code));
+        }
+    }
+}
