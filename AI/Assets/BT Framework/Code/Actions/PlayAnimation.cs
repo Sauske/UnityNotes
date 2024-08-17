6536f54aa389839c50e9c48fecf963ef6e88b520
Sauske <254862688@qq.com>
2024-08-17 14:04:30 +0800

    BDTree

---
+++ AI/Assets/BT Framework/Code/Actions/PlayAnimation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using BT;
+
+namespace BT.Sample
+{
+	public class PlayAnimation : BTAction
+	{
+		private string _animationName;
+
+
+		public PlayAnimation(string animationName, BTPrecondition precondition = null) : base(precondition)
+		{
+			_animationName = animationName;
+		}
+
+		protected override void Enter()
+		{
+			Animator animator = database.GetComponent<Animator>();
+			animator.Play(_animationName);
+		}
+	}
+}
