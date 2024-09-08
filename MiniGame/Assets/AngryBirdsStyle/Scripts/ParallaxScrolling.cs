354529cd3275df8629eec85aaedc6736fc1d1479
Sauske <254862688@qq.com>
2024-09-08 14:20:40 +0800

    AngryBirdsStyle

---
+++ MiniGame/Assets/AngryBirdsStyle/Scripts/ParallaxScrolling.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxScrolling : MonoBehaviour
+{
+    Camera mCamera;
+    // Use this for initialization
+    void Start()
+    {
+        mCamera = Camera.main;
+        previousCameraTransform = mCamera.transform.position;
+    }
+
+    /// <summary>
+    /// similar tactics just like the "CameraMove" script
+    /// </summary>
+    void Update()
+    {
+        Vector3 delta = mCamera.transform.position - previousCameraTransform;
+        delta.y = 0; delta.z = 0;
+        transform.position += delta / ParallaxFactor;
+
+
+        previousCameraTransform = mCamera.transform.position;
+    }
+
+    public float ParallaxFactor;
+
+    Vector3 previousCameraTransform;
+
+    ///background graphics found here:
+    ///http://opengameart.org/content/hd-multi-layer-parallex-background-samples-of-glitch-game-assets
+}
