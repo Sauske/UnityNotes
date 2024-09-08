27b07ec8875f1117c23ae8f488aff9a50d4c00c3
Sauske <254862688@qq.com>
2024-09-08 15:57:21 +0800

    MINI-BubbleBreaker

---
+++ MiniGame/Assets/BubbleBreaker/Scripts/ShowHighScores.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ShowHighScores : MonoBehaviour
+{
+
+    
+    // Use this for initialization
+    void Start()
+    {
+        DisplayScores();
+    }
+
+    private void DisplayScores()
+    {
+        Text txt = GameObject.Find("HighScores").GetComponent<Text>();
+
+
+        if (Globals.GameScore != 0)
+            txt.text = "Your score: " + Globals.GameScore + "\n";
+        else
+            txt.text = string.Empty;
+
+        foreach (var score in manager.GetScores())
+        {
+            txt.text += string.Format("{0} - {1}", score.Date.ToShortDateString(), score.ScoreInt) + "\n";
+        }
+    }
+
+    public ScoreManager manager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            SceneManager.LoadScene("StartScene");
+    }
+
+    void Awake()
+    {
+        manager = new ScoreManager();
+    }
+
+   
+}
---
+++ MiniGame/Assets/BubbleBreaker/Scripts/StartScene.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour
+{
+  
+    // Use this for initialization
+    void Start()
+    {
+        Globals.GameScore = 0;
+        (GameObject.Find("SoundToggle").GetComponent<Toggle>()).isOn = SettingsManager.Sound;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+
+    public void GoToGameScene()
+    {
+        SceneManager.LoadScene("bubbleBreakerGameScene");
+    }
+
+    public void GoToHighScoreScene()
+    {
+        SceneManager.LoadScene("highScoresScene");
+    }
+
+    public void SetSound(bool value)
+    {
+        SettingsManager.Sound = value;
+    }
+
+   
+
+
+}
